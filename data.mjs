ff9598af1e717d237cdc38a0f50325c57415f9c4
Ifeoma <osegboifeoma3@gmail.com>
2026-03-13 22:06:45 +0100

    Added a new file data.mjs to store preset data for visuals

---
+++ data.mjs
@@ -0,0 +1,14 @@
+export const presetTopics = {
+  useruno: [
+    { topic: "HTML Basics", date: "2026-03-01" },
+    { topic: "CSS Layouts", date: "2026-03-05" },
+  ],
+
+  userDos: [{ topic: "JavaScript Arrays", date: "2026-03-03" }],
+
+  userTres: [],
+
+  userCuatro: [{ topic: "Web Accessibility", date: "2026-03-02" }],
+
+  userCinco: [],
+};

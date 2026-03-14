930bea16ba07cadf2470348ac9bb20918bd25c39
Ifeoma <osegboifeoma3@gmail.com>
2026-03-14 22:51:14 +0100

    adjusted the dates

--- data.mjs
+++ data.mjs
@@ -1,14 +1,14 @@
 export const presetTopics = {
   user1: [
-    { topic: "HTML Basics", date: "2026-03-01" },
-    { topic: "CSS Layouts", date: "2026-03-05" },
+    { topic: "HTML Basics", date: "2027-03-01" },
+    { topic: "CSS Layouts", date: "2027-03-05" },
   ],
 
-  user2: [{ topic: "JavaScript Arrays", date: "2026-03-03" }],
+  user2: [{ topic: "JavaScript Arrays", date: "2027-03-03" }],
 
   user3: [],
 
-  user4: [{ topic: "Web Accessibility", date: "2026-03-02" }],
+  user4: [{ topic: "Web Accessibility", date: "2027-03-02" }],
 
   user5: [],
 };
